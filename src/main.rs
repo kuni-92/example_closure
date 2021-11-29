52d44f3f2ee8878803eee13fc2847aef21fde957
kunikuni03 <49485046+kunikuni03@users.noreply.github.com>
2021-11-29 22:30:32 +0900

    second refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,50 @@
 use std::thread;
 use std::time::Duration;
 
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!("calculation slowly...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
+struct Casher<T>
+    where T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Casher<T>
+    where T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Casher<T> {
+        Casher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            },
+        }
+    }
 }
 
 fn generate_workout(intensity: u32, random_value: u32) {
-    let expensive_result = simulated_expensive_calculation(intensity);
+    let mut expensive_result = Casher::new(|num| {
+        println!("calculation slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
 
     if intensity < 25 {
          println!(
             "Today, do {} push up!",
-            expensive_result
+            expensive_result.value(intensity)
         );
         println!(
             "Next, do {} shitups!",
-            expensive_result
+            expensive_result.value(intensity)
         );
     } else {
         if random_value == 3 {
@@ -25,7 +52,7 @@ fn generate_workout(intensity: u32, random_value: u32) {
         } else {
             println!(
                 "Today, ran for{} minutes!",
-                expensive_result
+                expensive_result.value(intensity)
             );
         }
     }
